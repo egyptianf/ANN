b391ebca98ef24048a544a2ba0c3b1383efe174a
egyptianf <walied2@protonmail.com>
2019-12-26 02:24:09 +0200

    vectorized activation function

--- NN.py
+++ NN.py
@@ -1,4 +1,5 @@
 import numpy as np
+import activation as ac
 
 
 class NeuralNetwork:
--- activation.py
+++ activation.py
@@ -1,5 +1,6 @@
 import abc
 import math
+import numpy as np
 
 
 class Activation:
@@ -12,32 +13,41 @@ class Activation:
 
 
 class Strategy(metaclass=abc.ABCMeta):
-    y = 0
+    y: 'numpy array'
+    y = None
 
     @abc.abstractmethod
-    def activate(self, x_):
+    def activate(self, x_: 'numpy array'):
         pass
 
 
 class Sigmoid(Strategy):
     # Implement the algorithm using the strategy interface
     def activate(self, x_):
-        self.y = 1 / (1 + math.exp(-x_))
+        self.y = np.zeros(len(x_))
+        self.y = 1 / (1 + np.exp(-x_))
 
 
 class ReLU(Strategy):
     # Implementation
     def activate(self, x_):
-        self.y = 0 if x_ < 0 else x_
+        self.y = np.zeros(len(x_))
+        for i in range(len(x_)):
+            self.y[i] = 0 if x_[i] < 0 else 1
 
 
-def main():
+def test():
     sigmoid = Sigmoid()
-    activation = Activation(sigmoid)
-    activation.activate(5)
-    print(activation.strategy.y)
+    activation1 = Activation(sigmoid)
+    activation1.activate(np.ones(5))
+    print("Sigmoid")
+    print(activation1.strategy.y)
+    print("ReLU")
+    relu = ReLU()
+    activation2 = Activation(relu)
+    activation2.activate(np.ones(5))
+    print(activation2.strategy.y)
 
 
-if __name__=='__main__':
-    print("hi")
-    main()
+if __name__ == '__main__':
+    test()
